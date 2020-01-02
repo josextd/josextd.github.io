6050712d61fb5374ae75f7bfb744be656414c6ec
Artal Bru <jrartal@minsait.com>
2020-01-02 17:44:39 +0100

    Updates

---
+++ precache-manifest.a9d5eb866ee0034192c98b686ec1df00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c643f127049737b20f070c90ea6bad",
+    "url": "/josextd.github.io/robofriends/index.html"
+  },
+  {
+    "revision": "520c36025f7a96a4099b",
+    "url": "/josextd.github.io/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "67aaf7b5bf870ad763fa",
+    "url": "/josextd.github.io/robofriends/static/css/main.2a173daa.chunk.css"
+  },
+  {
+    "revision": "520c36025f7a96a4099b",
+    "url": "/josextd.github.io/robofriends/static/js/2.43f9ce79.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/josextd.github.io/robofriends/static/js/2.43f9ce79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67aaf7b5bf870ad763fa",
+    "url": "/josextd.github.io/robofriends/static/js/main.9fa236f2.chunk.js"
+  },
+  {
+    "revision": "544c5bfdd92ec8ebbe07",
+    "url": "/josextd.github.io/robofriends/static/js/runtime-main.00c7926d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/josextd.github.io/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
