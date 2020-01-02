586d2e9cd42ac2f2a95b5f67295859bb568ce10a
Artal Bru <jrartal@minsait.com>
2020-01-02 17:42:25 +0100

    todo

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Anna Smith',
+    username: 'asmith',
+    email: 'AnnaSmith@apriil.biz'
+},
+{
+    id: 2,
+    name: 'Leanne Graham2',
+    username: 'Bret2',
+    email: 'Sincere2@apriil.biz'
+},
+{
+    id: 3,
+    name: 'Leanne Graham3',
+    username: 'Bret3',
+    email: 'Sincere3@apriil.biz'
+},
+{
+    id: 4,
+    name: 'Leanne Graham4',
+    username: 'Bret4',
+    email: 'Sincere4@apriil.biz'
+},
+{
+    id: 5,
+    name: 'Leanne Graham5',
+    username: 'Bret5',
+    email: 'Sincere5@apriil.biz'
+},
+{
+    id: 6,
+    name: 'Leanne Graham6',
+    username: 'Bret6',
+    email: 'Sincere6@apriil.biz'
+},
+{
+    id: 7,
+    name: 'Leanne Graham7',
+    username: 'Bret7',
+    email: 'Sincere7@apriil.biz'
+},
+{
+    id: 8,
+    name: 'Leanne Graham8',
+    username: 'Bret8',
+    email: 'Sincere8@apriil.biz'
+},
+{
+    id: 9,
+    name: 'Leanne Graham9',
+    username: 'Bret9',
+    email: 'Sincere9@apriil.biz'
+},
+{
+    id: 10,
+    name: 'Leanne Graham10',
+    username: 'Bret10',
+    email: 'Sincere10@apriil.biz'
+}
+]
